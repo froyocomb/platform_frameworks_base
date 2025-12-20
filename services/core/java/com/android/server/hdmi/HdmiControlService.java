a4c8f2e731e4493ecbfb2456a2e2cf628276ed01
scamdisk <158341159+scamdisk@users.noreply.github.com>
2025-12-20 16:41:53 +0200

    manual cherrypicking of 781041239f2931ca16c902fb371cd041b057c918

--- services/core/java/com/android/server/hdmi/HdmiControlService.java
+++ services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -109,7 +109,7 @@ public final class HdmiControlService extends SystemService {
     private HdmiCecController mCecController;
 
     @Nullable
-    private HdmiMhlController mMhlController;
+    private HdmiMhlControllerStub mMhlController;
 
     // Whether ARC is "enabled" or not.
     // TODO: it may need to hold lock if it's accessed from others.
@@ -134,7 +134,7 @@ public final class HdmiControlService extends SystemService {
             Slog.i(TAG, "Device does not support HDMI-CEC.");
         }
 
-        mMhlController = HdmiMhlController.create(this);
+        mMhlController = HdmiMhlControllerStub.create(this);
         if (mMhlController == null) {
             Slog.i(TAG, "Device does not support MHL-control.");
         }
