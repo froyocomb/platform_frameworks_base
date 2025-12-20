b6c838381e9f060d52a775198d48e64b5221d5d8
scamdisk <158341159+scamdisk@users.noreply.github.com>
2025-12-20 16:41:53 +0200

    HDMI MHL changes - Lollipop
    Squash these for further cherrypicking

--- services/core/java/com/android/server/hdmi/HdmiControlService.java
+++ services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -115,7 +115,7 @@ public final class HdmiControlService extends SystemService {
     private HdmiCecController mCecController;
 
     @Nullable
-    private HdmiMhlController mMhlController;
+    private HdmiMhlControllerStub mMhlController;
 
     // HDMI port information. Stored in the unmodifiable list to keep the static information
     // from being modified.
@@ -138,7 +138,7 @@ public final class HdmiControlService extends SystemService {
             Slog.i(TAG, "Device does not support HDMI-CEC.");
         }
 
-        mMhlController = HdmiMhlController.create(this);
+        mMhlController = HdmiMhlControllerStub.create(this);
         if (mMhlController == null) {
             Slog.i(TAG, "Device does not support MHL-control.");
         }
