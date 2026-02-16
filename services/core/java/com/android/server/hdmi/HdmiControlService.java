43f82230d74f412ab37366123289dca9251cb46a
main3782 <tlustymarcin5@gmail.com>
2026-02-16 12:12:15 +0100

    Remove MHL (build is too early in order for the stub to work properly)

--- services/core/java/com/android/server/hdmi/HdmiControlService.java
+++ services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -39,9 +39,6 @@ public final class HdmiControlService extends SystemService {
     @Nullable
     private HdmiCecController mCecController;
 
-    @Nullable
-    private HdmiMhlController mMhlController;
-
     public HdmiControlService(Context context) {
         super(context);
     }
@@ -52,11 +49,7 @@ public final class HdmiControlService extends SystemService {
         if (mCecController == null) {
             Slog.i(TAG, "Device does not support HDMI-CEC.");
         }
-
-        mMhlController = HdmiMhlController.create(this);
-        if (mMhlController == null) {
-            Slog.i(TAG, "Device does not support MHL-control.");
-        }
+        
     }
 
     /**
