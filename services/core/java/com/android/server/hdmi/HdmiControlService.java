f9fa215c05f83fe40910099fd6a7330fcb8bea2c
main3782 <tlustymarcin5@gmail.com>
2026-02-16 17:23:03 +0100

    Revert "Remove MHL (build is too early in order for the stub to work properly)"
    This reverts commit da92a1059521651812692fbf6f29815cc4c19262.

--- services/core/java/com/android/server/hdmi/HdmiControlService.java
+++ services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -48,6 +48,9 @@ public final class HdmiControlService extends SystemService {
     @Nullable
     private HdmiCecController mCecController;
 
+    @Nullable
+    private HdmiMhlController mMhlController;
+
     public HdmiControlService(Context context) {
         super(context);
     }
@@ -57,6 +60,11 @@ public final class HdmiControlService extends SystemService {
         mCecController = HdmiCecController.create(mIoThread.getLooper(), mHandler);
         if (mCecController == null) {
             Slog.i(TAG, "Device does not support HDMI-CEC.");
+        }
+
+        mMhlController = HdmiMhlController.create(mIoThread.getLooper(), mHandler);
+        if (mMhlController == null) {
+            Slog.i(TAG, "Device does not support MHL-control.");
         }
     }
 }
--- services/core/jni/onload.cpp
+++ services/core/jni/onload.cpp
@@ -40,6 +40,7 @@ int register_android_server_connectivity_Vpn(JNIEnv* env);
 int register_android_server_dreams_McuHal(JNIEnv* env);
 int register_android_server_hdmi_HdmiCecController(JNIEnv* env);
 int register_android_server_hdmi_HdmiCecService(JNIEnv* env);
+int register_android_server_hdmi_HdmiMhlController(JNIEnv* env);
 };
 
 using namespace android;
@@ -76,6 +77,7 @@ extern "C" jint JNI_OnLoad(JavaVM* vm, void* reserved)
     register_android_server_hdmi_HdmiCecController(env);
     // TODO: remove this once replaces HdmiCecService with HdmiControlService.
     register_android_server_hdmi_HdmiCecService(env);
+    register_android_server_hdmi_HdmiMhlController(env);
 
     return JNI_VERSION_1_4;
 }
