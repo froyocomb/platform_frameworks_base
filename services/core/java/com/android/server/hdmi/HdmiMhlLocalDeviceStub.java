c0845eb9b127955f623d2e79cdbff2f13b61ae58
main3782 <tlustymarcin5@gmail.com>
2026-02-19 14:56:46 +0100

    Removing a lot of stuff not yet added

--- services/core/java/com/android/server/hdmi/HdmiMhlLocalDeviceStub.java
+++ services/core/java/com/android/server/hdmi/HdmiMhlLocalDeviceStub.java
@@ -1,12 +1,9 @@
 package com.android.server.hdmi;
-import android.hardware.hdmi.HdmiDeviceInfo;
 import android.hardware.hdmi.IHdmiControlCallback;
 /**
  * Stub class that models a logical mhl device hosted in this system.
  */
 final class HdmiMhlLocalDeviceStub {
-    private static final HdmiDeviceInfo INFO = new HdmiDeviceInfo(
-            Constants.INVALID_PHYSICAL_ADDRESS, Constants.INVALID_PORT_ID, -1, -1);
     private final HdmiControlService mService;
     private final int mPortId;
     protected HdmiMhlLocalDeviceStub(HdmiControlService service, int portId) {
@@ -14,9 +11,6 @@ final class HdmiMhlLocalDeviceStub {
         mPortId = portId;
     }
     void onDeviceRemoved() {
-    }
-    HdmiDeviceInfo getInfo() {
-        return INFO;
     }
     void setBusMode(int cbusmode) {
     }
